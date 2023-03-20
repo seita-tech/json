7774a43038c74d75cd192af651f60c9115510840
seita-tech <127118200+seita-tech@users.noreply.github.com>
2023-03-20 09:20:55 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./styles.css";
+import axios from "axios";
+
+export default function App() {
+  const onClickUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const onClickUser1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users?id=1")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="App">
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のuser</button>
+    </div>
+  );
+}
